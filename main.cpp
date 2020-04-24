8cca9fde0b5ec8ad28080b593095954df2b1d56c
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-04-24 23:30:55 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,22 @@
 using namespace std;
 
 // main.cpp file for main game playing
-int main(){
-  char input;
-  // display the option that user can pick.
-  cout << "input N to start a New Game" << endl;
-  cout << "input L to load Game" << endl;
-  cout << "input E to exit the Game" << endl;
-  // get the user input
-  cin >> input;
-  // if ( n == 'N')
-  
-  // if ( n == 'L')
-  
-  else if (input == 'E'){
-    // temporary
-    exit(1);
-  }
-  return 0;
+int main() {
+    char input;
+    // display the option that user can pick.
+    cout << "input N to start a New Game" << endl;
+    cout << "input L to load Game" << endl;
+    cout << "input E to exit the Game" << endl;
+    // get the user input
+    cin >> input;
+    // if ( n == 'N')
+
+    // if ( n == 'L')
+
+    else if (input == 'E') {
+        cout << "BYE BYE" << endl;
+        // temporary
+        exit(1);
+    }
+    return 0;
 }
