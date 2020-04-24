f673a07b1893164901b44d85567755a7bbdb0213
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-04-24 23:09:37 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 // main.cpp file for main game playing
 int main(){
-  
+  string input;
+  // display the option that user can pick.
+  cout << "New Game" << endl;
+  cout << "Load Game" << endl;
+  cout << "Exit Game" << endl;
+  // get the user input
+  getline(cin, input);
   return 0;
 }
