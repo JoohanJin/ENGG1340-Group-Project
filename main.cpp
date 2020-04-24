ef0205b16e5fa1988c5599d66aeb9613ff8ff2a5
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-04-24 23:57:45 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 // main.cpp file for main game playing
 int main() {
     char input;
+    string name;
     // display the option that user can pick.
     cout << "input N to start a New Game" << endl;
     cout << "input L to load Game" << endl;
     cout << "input E to exit the Game" << endl;
+
     // get the user input
     cin >> input;
-    // if ( n == 'N')
 
-    // if ( n == 'L')
+    // if ( input == 'N'), Make a new character
+    if (input = 'N') {
+        getline(cin, name);
+        ofstream output;
+        output.open("");
 
+    }
+
+    // if ( input == 'L'), load character file
+
+    // if ( input == 'E'), terminate the program
     else if (input == 'E') {
         cout << "BYE BYE" << endl;
         // temporary
         exit(1);
     }
+
+
     return 0;
 }
