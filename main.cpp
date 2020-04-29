ecbc0dea049f02537d772152c1779c7e0e1d3d1c
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-04-29 11:21:55 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "random.h"
 
 using namespace std;
 
 int main() {
     char input;
     // job = class
-    string name, job = "Knight";
+    string name, job, C_status = "Player_status.txt";
     int skill_number(2);
     // display the option that user can pick.
     cout << "input N to start a New Game" << endl;
@@ -23,15 +24,30 @@ int main() {
         if (input == 'N') {
             cout << "Enter Your Name (your name should be one word): ";
             cin >> name;
+            job = character_class();
             ofstream character;
-            character.open("Character/Player_status.txt");
+            character.open(C_status);
             character << name << endl;
             character << job << endl;
             character << skill_number << endl;
+            character.close();
             break;
         }
 
         // if ( input == 'L'), load character file
+        else if (input == 'L') {
+            ifstream character(C_status);
+            // if Player_status.txt exist --> load the text file
+            if (character.fail()) {
+                cout << "No Character Exists" << endl;
+                cin >> input;
+            }
+
+            // if Player_status.txt does not exist --> print error message and get the input one more time
+            else {
+
+            }
+        }
 
         // if ( input == 'E'), terminate the program
         else if (input == 'E') {
