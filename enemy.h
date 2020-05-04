8bb2518573ad2fb65cfc4c542ed7538c64c4e741
Andy Bahk Jr <andybahk@connect.hku.hk>
2020-05-05 00:36:46 +0900

    enemy.h added level

--- enemy.h
+++ enemy.h
@@ -9,6 +9,7 @@ int main() {
 		string moveA;
 		string moveB;
 		string moveC;
+		int level;
 	};
 
 	Enemy enemy_information(string name) {
@@ -16,28 +17,33 @@ int main() {
 		if (name == Sulyvahn) {
 			enemy.hp = 20000;
 			enemy.moveA = "Fireball";
-			enemy.moveB = "Eclipse"
+			enemy.moveB = "Eclipse";
+			enemy.level = 1;
 		}
 		else if (name == Lorian) {
 			enemy.hp = 30000;
 			enemy.moveA = "Stab";
-			enemy.moveB = "Slash"
+			enemy.moveB = "Slash";
+			enemy.level = 2;
 		}
 		else if (name == Lothric) {
 			enemy.hp = 25000;
 			enemy.moveA = "Curse";
-			enemy.moveB = "Shadow"
+			enemy.moveB = "Shadow";
+		
 		}
 		else if (name == Artorias) {
 			enemy.hp = 40000;
 			enemy.moveA = "Dance";
-			enemy.moveB = "Whisper"
+			enemy.moveB = "Whisper";
+			enemy.level = 3;
 		}
 		else if (name == Manus) {
 			enemy.hp = 50000;
 			enemy.moveA = "Clutches";
 			enemy.moveB = "Downward";
 			enemy.moveC = "Last";
+			enemy.level = 4;
 		}
 	}
 	
