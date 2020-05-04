5ed7ba810af26af7d93e3348eb8409baf90c20bf
Andy Bahk Jr <andybahk@connect.hku.hk>
2020-05-05 00:24:08 +0900

    Enemy.h add

---
+++ enemy.h
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+int main() {
+	struct Enemy {
+		string name;
+		int hp;
+		string moveA;
+		string moveB;
+	};
+
+	Enemy enemy_info(string name) {
+		Enemy enemy;
+
+		if (name == Sulyvahn) {
+			enemy.hp = 20000;
+			enemy.moveA = "Fireball";
+			enemy.moveB = "Eclipse"
+		}
+		else if (name == Lorian) {
+			enemy.hp = 30000;
+			enemy.moveA = "Stab";
+			enemy.moveB = "Slash"
+		}
+		else if (name == Lothric) {
+			enemy.hp = 25000;
+			enemy.moveA = "Curse";
+			enemy.moveB = "Shadow"
+		}
+		else if (name == Artorias) {
+			enemy.hp = 40000;
+			enemy.moveA = "Dance";
+			enemy.moveB = "Whisper"
+		}
+		else if (name == Manus) {
+			enemy.hp = 50000;
+			enemy.moveA = "Clutches";
+			enemy.moveB = "Downward";
+			enemy.moveC = "Last";
+		}
+	}
+	
+
+	
+}
