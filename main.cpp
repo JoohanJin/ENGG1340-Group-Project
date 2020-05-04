ff2f263956c6b71f7dd2f5076a9284099bb7798e
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-05-05 00:52:38 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,69 +1,16 @@
 #include <iostream>
 #include <string>
-#include <fstream>
-// import randomized character selection function into main function
-#include "random.h"
+// import randomized character selection function into main function.
+#include "Gettinginput.h"
+#include "Starting_game.h"
 
 using namespace std;
 
 int main() {
-    char input;
+    char input = getting_input();
     // job = class
     string name, job, C_status = "Player_status.txt";
-    int skill_number(2);
-    // display the option that user can pick.
-    cout << "input N to start a New Game" << endl;
-    cout << "Previous savefile (if there is any) will be removed" << endl;
-    cout << "input L to load Game" << endl;
-    cout << "input E to exit the Game" << endl;
-
-    // get the user input
-    cin >> input;
-
-    while (true) {
-        // if ( input == 'N'), Make a new character
-        if (input == 'N') {
-            cout << "Enter Your Name (your name should be one word): ";
-            cin >> name;
-            job = character_class();
-            ofstream character;
-            character.open(C_status);
-            character << name << endl;
-            character << job << endl;
-            character << skill_number << endl;
-            character.close();
-            break;
-        }
-
-        // if ( input == 'L'), load character file
-        else if (input == 'L') {
-            ifstream character(C_status);
-            // if Player_status.txt does not exist --> print error message and get the input one more time
-            if (character.fail()) {
-                cout << "No Character Exists" << endl;
-                cin >> input;
-            }
-
-            // if Player_status.txt exist --> load the text file
-            else {
-
-            }
-        }
-
-        // if ( input == 'E'), terminate the program
-        else if (input == 'E') {
-            cout << "BYE BYE" << endl;
-            break;
-            // temporary
-            exit(1);
-        }
-
-        else {
-            cout << "invalid input" << endl;
-            cout << "Input again: ";
-            cin >> input;
-        }
-    }
-
+    int level(1), skill_number(2);
+    starting_game(input, level, skill_number, job, C_status);
     return 0;
 }
