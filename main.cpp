6498c650645895f729e6b2020d8a067539efff28
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-05-07 03:22:27 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,5 +15,10 @@ int main() {
     string name, job, C_status = "Player_status.txt";
     int level(1), skill_number(2);
     starting_game(input, level, skill_number, job, C_status, name, s);
+
+    cout << "You currently can use" << endl;
+    for (int i = 0; i < skill_number; i++) {
+        cout << s[i].name << ": " << s[i].explanation << endl;
+    }
     return 0;
 }
