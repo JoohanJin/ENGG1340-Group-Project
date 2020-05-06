8c24847baa8a7f53953bc653bf0f96581e6a6aa9
Andy Bahk Jr <andybahk@connect.hku.hk>
2020-05-07 00:48:17 +0900

    Made name in enemy.h string added to fighting function

--- enemy.h
+++ enemy.h
@@ -8,47 +8,40 @@ int main() {
 		int hp;
 		string move;
 		int damage;
-		int level;
 		int healing;
 	};
 
 	Enemy enemy_information(string name) {
 		Enemy enemy[5];
-		if (name == Sulyvahn) {
+		if (name == "Sulyvahn") {
 			enemy[0].hp = 20000;
-			enemy[1].level = 1;
-			enemy[2].move = "Fireball"; enemy[1].damage = 500;
-			enemy[3].move = "Eclipse";  enemy[2].damage = 2000;
+			enemy[1].move = "Fireball"; enemy[1].damage = 500;
+			enemy[2].move = "Eclipse";  enemy[2].damage = 2000;
 			
 		}
-		else if (name == Lorian) {
+		else if (name == "Lorian") {
 			enemy[0].hp = 30000;
-			enemy[1].level = 2;
-			enemy[2].move = "Stab"; enemy[1].dmg = 1000;
-			enemy[3].move = "Slash"; enemy[2].dmg = 2000;
+			enemy[1].move = "Stab"; enemy[1].dmg = 1000;
+			enemy[2].move = "Slash"; enemy[2].dmg = 2000;
 			
 		}
-		else if (name == Lothric) {
+		else if (name == "Lothric") {
 			enemy[0].hp = 25000;
-			enemy[1].level = 3;
-			enemy[1].move = "Curse";
-			enemy[2].move = "Shadow";
+			enemy[1].move = "Curse"; enemy[1].damage = 1000;
+			enemy[2].move = "Shadow"; enemy[2].damage = 2000; enemy[2].healing = 1000;
 		
 		}
-		else if (name == Artorias) {
+		else if (name == "Artorias") {
 			enemy[0].hp = 40000;
-			enemy[1].level = 4;
-			enemy[2].move = "Dance"; enemy[1].damage = 1000;
-			enemy[3].move = "Whisper"; enemy[2].damage = 2000; enemy[2].healing = 1000;
+			enemy[1].move = "Dance"; enemy[1].damage = 1000;
+			enemy[2].move = "Whisper"; enemy[2].damage = 5000; 
 			
 		}
-		else if (name == Manus) {
+		else if (name == "Manus") {
 			enemy[0].hp = 50000;
-			enemy[1].level = 5;
-			enemy[2].move = "Clutches"; enemy[2].damage = 1000;
-			enemy[3].move = "Downward"; enemy[3].damage = 2000;
-			enemy[4].move = "Last"; enemy[4].damage = 30000;
-			
+			enemy[1].move = "Clutches"; enemy[2].damage = 1000;
+			enemy[2].move = "Downward"; enemy[3].damage = 2000;
+			enemy[3].move = "Last"; enemy[4].damage = 30000;
 		}
 	}
 	
--- fighting.h
+++ fighting.h
@@ -8,11 +8,45 @@ using namespace std;
 
 
 
-void fighting(int &level) {
-	enemy information = enemy_information(level);
-	string winner = "None", loser = "None";
-	bool playing = true;
 
+void fighting(int &level) {
+	string winner = "None", loser = "None";
+	string name = "";
+	bool playing = true;
+	// getting the information of enemy from the enemy.txt about skills
+	// if the level is 1, get the information about pontiff Sulyvahn
+	if (level == 1) {
+		name = "Sulyvahn";
+		enemy_information(name);
+
+	}
+
+	// if the level is 2, get the information about Lorian
+	else if (level == 2) {
+		name = "Lorian";
+		enemy_information(name);
+
+	}
+
+	// if the level is 3, get the information about Lothric
+	else if (level == 3) {
+		name = "Lothric";
+		enemy_information(name);
+
+	}
+
+	// if the level is 4, get the information about Artorias
+	else if (level == 4) {
+		name = "Artorias";
+		enemy_information(name);
+
+	}
+	// if the level is 3, get the information about Manus
+	else if (level == 5) {
+		name = "Manus";
+		enemy_information(name);
+
+	}
 	// Basic Fighting system
 	while (playing) {
 		// randomize --> skill
