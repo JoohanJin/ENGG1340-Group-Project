0d3da2f8fae6860761f2f2f8569a4b310b1a5542
Andy Bahk Jr <andybahk@connect.hku.hk>
2020-05-07 00:33:51 +0900

    deleted enemy_info from fighting.h

--- fighting.h
+++ fighting.h
@@ -8,34 +8,6 @@ using namespace std;
 
 
 
-enemy enemy_information(int level) {
-	// getting the information of enemy from the enemy.txt about skills
-	// if the level is 1, get the information about pontiff Sulyvahn
-	if (level == 1) {
-
-
-	}
-
-	// if the level is 2, get the information about Lorian
-	else if (level == 2) {
-
-	}
-
-	// if the level is 3, get the information about Lothric
-	else if (level == 3) {
-
-	}
-
-	// if the level is 4, get the information about Artorias
-	else if (level == 4) {
-
-	}
-	// if the level is 3, get the information about Manus
-	else if (level == 5) {
-
-	}
-}
-
 void fighting(int &level) {
 	enemy information = enemy_information(level);
 	string winner = "None", loser = "None";
