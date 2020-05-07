e6dde83f0120a0443b1a5fd188edb83deb10d3b6
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-05-08 04:42:40 +0900

    Update fighting.h

--- fighting.h
+++ fighting.h
@@ -2,7 +2,6 @@
 #include <string>
 #include <ctime>
 #include "enemy.h"
-#include "character.h"
 
 
 using namespace std;
@@ -12,6 +11,7 @@ void check_game_done(bool& playing, string winner, int& level, int& skill_number
 	//increment level
 	//loop for current level is done
 	if (winner == "Player") {
+		cout << "YOU HUNTED" << endl;
 		level++;
 		playing = false;
 	}
@@ -25,103 +25,265 @@ void check_game_done(bool& playing, string winner, int& level, int& skill_number
 	}
 }
 
+void check_game_done_hp(double enemy_hp, double player_hp, string& winner) {
+	if (enemy_hp <= 0) {
+		winner = "Player";
+	}
+
+	else if (player_hp <= 0) {
+		winner = "Enemy";
+	}
+}
+
+void printing_status(double enemy_hp, double player_hp) {
+	cout << "Your hp is " << player_hp << endl;
+	cout << "Enemy's hp is " << enemy_hp << endl;
+}
+
+double special_skill(int previous_skill, string job) {
+	if (previous_skill == 1) {
+		srand((int)time(0));
+		int number = rand() % 2;
+		if (job == "Knight") {
+			cout << "You block enemy's attack with your TITAN SHIELD!!!" << endl;
+			return 0.5;
+		}
+
+		else if (job == "Wizard") {
+			// when the armor is not broken
+			if (number == 0) {
+				cout << "Your armor is not broken, successfully reflect enemy's enemy!" << endl;
+				return -1;
+			}
+
+			// when the armor is broken
+			else if (number == 1) {
+				cout << "Your armor is broken!, you get more damage!" << endl;
+				return 1.5;
+			}
+		}
+
+		else if (job == "Theif") {
+			// when dodge was successful
+			if (number == 0) {
+				cout << "You successfully dodge enemy's attack!" << endl;
+				return 0;
+			}
+
+			// when dodge was not successful
+			else if (number == 1) {
+				cout << "You could not dodge enemy's attack!" << endl;
+				return 1;
+			}
+		}
+
+		else {
+			// healing already happened
+			return 1;
+		}
+	}
+
+	else if (previous_skill != 1) {
+		return 1;
+	}
+}
+
+void calculating_damage(int random_number, int previous_skill, string job, Enemy e[5], int level, double& damage, double& healing, string name) {
+	double number;
+	//modification of percentage
+	if (level == 5) {
+		// Clutches of The Abyss
+		if (random_number < 20) {
+			cout << "Manus uses Clutches of the Abyss!" << endl;
+			damage = e[1].damage;
+			number = special_skill(previous_skill, job);
+			damage *= number;
+		}
+
+		// Downward Blow
+		else if (random_number >= 20 && random_number < 95) {
+			cout << "Manus uses Downward Blow" << endl;
+			damage = e[2].damage;
+			number = special_skill(previous_skill, job);
+			damage *= number;
+		}
+
+		// Last Resort
+		else if (random_number >= 95) {
+			cout << "Manus uses Last Resort!" << endl;
+			cout << "THIS ATTACK WILL BE THE LAST THING YOU CAN SEE BEFORE YOU DIE!!!!" << endl;
+			damage = 30000;
+			number = special_skill(previous_skill, job);
+			damage *= number;
+		}
+	}
+
+	//percentage for the 3 mini bosses
+	else if (level != 5 && level != 3) {
+		// special attack
+		if (random_number < 20) {
+			cout << name << " uses " << e[2].move << endl;
+			damage = e[2].damage;
+			number = special_skill(previous_skill, job);
+			damage *= number;
+		}
+		// normal
+		else if (random_number >= 20) {
+			cout << name << " uses " << e[1].move << endl;
+			damage = e[1].damage;
+			number = special_skill(previous_skill, job);
+			damage *= number;
+		}
+	}
+
+	// Lothric who has healing magic
+	else if (level == 3) {
+		// special --> also has healing
+		if (random_number < 20) {
+			cout << name << " uses " << e[2].move << endl;
+			damage = e[2].damage;
+			number = special_skill(previous_skill, job);
+			damage *= number;
+			healing = e[2].healing;
+		}
+
+		// normal
+		else if (random_number >= 20) {
+			cout << name << " uses " << e[1].move << endl;
+			damage = e[1].damage;
+			number = special_skill(previous_skill, job);
+			damage *= number;
+		}
+	}
+}
+
+
+
 void fighting(int& level, int& skill_number, string job, Skill s[4], string& winner, Enemy e[5]) {
-	Skill s[4];
-	string name;
+	string name, current_player("Enemy");
+	int previous_skill = 0, Max_hp;
 	bool playing = true;
-	int player_hp, enemy_hp;
+	double player_hp, enemy_hp, damage;
 	// getting the information of enemy from the enemy.h about skills
 	// if the level is 1, get the information about pontiff Sulyvahn
 	if (level == 1) {
 		name = "Sulyvahn";
 		enemy_information(name, e);
-		if (player_hp <= 0) {
-			loser = "Player";
-		}
-		else if (enemy_hp <= 0) {
-			winner = "Player";
-		}
+		Max_hp = 5000;
+		player_hp = 5000;
+		enemy_hp = e[0].enemy_hp;
 	}
 
 	// if the level is 2, get the information about Lorian
 	else if (level == 2) {
 		name = "Lorian";
 		enemy_information(name, e);
-		if (player_hp <= 0) {
-			loser = "Player";
-		}
-		else if (enemy_hp <= 0) {
-			winner = "Player";
-		}
-
+		Max_hp = 10000;
+		player_hp = 10000;
+		enemy_hp = e[0].enemy_hp;
 	}
 
 	// if the level is 3, get the information about Lothric
 	else if (level == 3) {
 		name = "Lothric";
 		enemy_information(name, e);
-		if (player_hp <= 0) {
-			loser = "Player";
-		}
-		else if (enemy_hp <= 0) {
-			winner = "Player";
-		}
-
+		Max_hp = 10000;
+		player_hp = 10000;
+		enemy_hp = e[0].enemy_hp;
+		skill_number++;
 	}
 
 	// if the level is 4, get the information about Artorias
 	else if (level == 4) {
 		name = "Artorias";
 		enemy_information(name, e);
-		if (player_hp <= 0) {
-			loser = "Player";
-		}
-		else if (enemy_hp <= 0) {
-			winner = "Player";
-		}
-
+		Max_hp = 15000;
+		player_hp = 15000;
+		enemy_hp = e[0].enemy_hp;
 	}
 	// if the level is 5, get the information about Manus
 	else if (level == 5) {
 		name = "Manus";
 		enemy_information(name, e);
-		if (player_hp <= 0) {
-			loser = "Player";
-		}
-		else if (enemy_hp <= 0) {
-			winner = "Player";
-		}
-
+		Max_hp;
+		player_hp = 20000;
+		enemy_hp = e[0].enemy_hp;
+		skill_number++;
 	}
 
 	// Basic Fighting system
 	while (playing) {
-		// randomize --> skill
-		int random_number;
-		srand((int)time(0));
-		random_number = rand() % 100;
-		//case of if enemy is Manus who has an extra skill
-		//modification of percentage
-		if (level == 5) {
-			if (random_number > 80) {
-				
+		double healing = 0;
+		// handling the turn
+		if (current_player == "Enemy") {
+			// 
+			srand((int)time(0));
+			int random_number = rand() % 100;
+			//case of if enemy is Manus who has an extra skill
+			calculating_damage(random_number, previous_skill, job, e, level, damage, healing, name);
+			if (healing != 0) {
+				enemy_hp += healing;
 			}
-			else if (random_number > 5 and random_number <= 80) {
+			if (damage < 0) {
+				enemy_hp -= damage;
+			}
+			else if (damage >= 0) {
+				player_hp -= damage;
+			}
 
-			}
-			else if (random_number = < 5) {
+			if (enemy_hp > e[0].enemy_hp)
+				enemy_hp = e[0].enemy_hp;
 
-			}
+			printing_status(enemy_hp, player_hp);
+
+			current_player = "Player";
+
+			cout << endl;
 		}
-		//percentage for the 4 mini bosses
-		else if (level != 5) {
-			if (random_number > 80) {
-
+		else if (current_player == "Player") {
+			//show the option first
+			//we assume that input is integer value only
+			int input;
+			cout << "It is your turn! Choose your skill (input the number of skill only!): " << endl;
+			for (int i = 0; i < skill_number; i++) {
+				cout << i + 1 << ". " << s[i].name << ": " << s[i].explanation << endl;
 			}
-			else if (random_number < 20) {
-
+			// we only get the integer input
+			cin >> input;
+			while (input < 1 || input-1 >= skill_number) {
+				cout << "invalid input! input again: ";
+				cin >> input;
 			}
+			// input is appropriate
+			input--;
+			while (previous_skill == 3 && input == 3) {
+				cout << "You already used your Ultimate! Cool time of one round is needed! Please choose other skill: ";
+				cin >> input;
+			}
+			previous_skill = input;
+			
+			cout << "You chose " << s[input].name << "!" << endl;
+
+			// healing skill
+			if (s[input].healing != 0) {
+				player_hp += player_hp * s[input].healing;
+				if (player_hp > Max_hp) {
+					player_hp = Max_hp;
+				}
+				printing_status(enemy_hp, player_hp);
+			}
+
+			// attack skill
+			else if (s[input].damage != 0) {
+				enemy_hp -= s[input].damage;
+				printing_status(enemy_hp, player_hp);
+			}
+			current_player = "Enemy";
+
+			cout << endl;
+			
 		}
+		check_game_done_hp(enemy_hp, player_hp, winner);
 		
 		check_game_done(playing, winner, level, skill_number);
 	}
