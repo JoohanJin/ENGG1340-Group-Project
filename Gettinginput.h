72af40bb74326484ac44d6974314fa1ae986a9fb
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-05-08 04:43:38 +0900

    Update Gettinginput.h

--- Gettinginput.h
+++ Gettinginput.h
@@ -1,16 +1,49 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-char getting_input() {
-	char input;
-    // display the option that user can pick.
-    cout << "input N to start a New Game" << endl;
-    cout << "Previous savefile (if there is any) will be removed" << endl;
-    cout << "input L to load Game" << endl;
-    cout << "input E to exit the Game" << endl;
-    cout << "input: ";
-    // get the user input
-    cin >> input;
-    return input;
+string character_class() {
+    //variable to generate random number
+    int randNum;
+    string Job;
+    srand((int)time(NULL));
+    //allow variation of number generation
+    //generation of random number
+    randNum = rand() % 5;
+    //switch-case statement to decide class
+    switch (randNum) {
+    case 0:
+        Job = "Knight";
+        cout << "You are a Knight" << endl;
+        cout << "An obscure knight of poor renown who collapsed roaming the land. Sturdy, owing to high vitality and stout armor." << endl;
+        return Job;
+        break;
+    case 1:
+        Job = "Paladin";
+        cout << "You are a Paladin" << endl;
+        cout << "A former paladin who journeyed to finish a quest undertaken. Wields a sturdy spear and employs a gentle restorative miracle." << endl;
+        return Job;
+        break;
+    case 2:
+        Job = "Priest";
+        cout << "You are a Priest" << endl;
+        cout << "A traveling cleric who collapsed from exhaustion. Channels high faith to cast many and varied miracles." << endl;
+        return Job;
+        break;
+    case 3:
+        Job = "Wizard";
+        cout << "You are a Wizard" << endl;
+        cout << "A loner who left formal academia to pursue further research. Commands soul wizards using high intelligence." << endl;
+        return Job;
+        break;
+    case 4:
+        Job = "Thief";
+        cout << "You are a Thief" << endl;
+        cout << "A common thief and pitiful deserter. Wields a dagger intended for backstabs alongside a military-issue bow." << endl;
+        return Job;
+        break;
+    }
 }
