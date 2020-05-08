5eff7d1a09d7d8e8076f8fa44580d6db329b1d8f
Andy Bahk Jr <andybahk@connect.hku.hk>
2020-05-09 02:50:39 +0900

    add

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main() {
     int level(1), skill_number(2);
     starting_game(input, level, skill_number, job, C_status, name, s);
 
-    cout << "You currently can use" << endl;
+    cout << "You currently can use:" << endl;
     for (int i = 0; i < skill_number; i++) {
         cout << s[i].name << ": " << s[i].explanation << endl;
     }
