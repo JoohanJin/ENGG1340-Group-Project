d45a710cb32c1dd140b5eef41c6ece1f4419c224
Andy Bahk Jr <andybahk@connect.hku.hk>
2020-05-09 00:11:43 +0900

    added and fixed descriptions

--- Character.h
+++ Character.h
@@ -52,8 +52,9 @@ void character_storing(string job, int skill_number, Skill s[4]) {
 		s[1].name = "Thorn Armor"; s[1].damage = 0; s[1].healing = 0; // 50% 1.5x damage and 50% reflect the damage
 		s[1].explanation = "Wearing  Thorn Armor that reflect the damage to enemy, but if the armor is broken, damage taken by you will be greater. you will either reflect damage or get 1.5x damage more.";
 		s[2].name = "Fireball"; s[2].damage = 8000; s[2].healing = 0;
-		s[3].explanation = "Wizard's special attack, summon hell fire from the hell. 8000 damage";
+		s[2].explanation = "Wizard's special attack, summon hell fire from the hell. 8000 damage";
 		s[3].name = "Lightning Bolt"; s[3].damage = 15000; s[3].healing = 0;
+		s[3].explanation = "Wizard's ultimate attack, summons lightning from the clouds. 8000 damage";
 	}
 
 	else if (job == "Thief") {
@@ -62,8 +63,8 @@ void character_storing(string job, int skill_number, Skill s[4]) {
 		s[1].name = "Dodge"; s[1].damage = 0; s[1].healing = 0;
 		s[1].explanation = "Dodge enemy's attack with quick step. You shell be careful otherwise, you will get hit. you will either dodge enemy's attack or get hit.";
 		s[2].name = "Poison Vile"; s[2].damage = 8000; s[2].healing = 0;
-		s[3].explanation = "Use poison, 8000 damage.";
+		s[2].explanation = "Use poison, 8000 damage.";
 		s[3].name = "Clone Attack"; s[3].damage = 15000; s[3].healing = 0;
-		s[3].explanation = "Summon Clone to attack twice. 15000 damage";
+		s[3].explanation = "Theif's ultimate attack, summon clone to attack twice. 15000 damage";
 	}
 }
