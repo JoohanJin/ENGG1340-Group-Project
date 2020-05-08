d13c425f31bd1020ed61c5b6380adfdf0d8851bc
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-05-09 00:19:50 +0900

    Update Title.h

--- Title.h
+++ Title.h
@@ -4,5 +4,12 @@ using namespace std;
 
 // design watever you like
 void print_title() {
-
+	cout << "////////////////////////////////////////////////////////////////////////////////" << endl;
+	cout << "///                                                                          ///" << endl;
+	cout << "///                        DARK SOULS - Texted Version                       ///" << endl;
+	cout << "///               (this game is inspired by Dark Souls series)               ///" << endl;
+	cout << "///          (name of enemy and classes are from Dark Souls 1 and 3)         ///" << endl;
+	cout << "///                          PREPARE TO DIE Edition                          ///" << endl;
+	cout << "///                                                                          ///" << endl;
+	cout << "////////////////////////////////////////////////////////////////////////////////" << endl;
 }
