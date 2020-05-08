3152e9a21d60ffb4858de0e7ea142bb1513a1226
JoohanJin <61616324+JoohanJin@users.noreply.github.com>
2020-05-09 01:26:04 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 #include <string>
 #include <fstream>
 #include <ctime>
+#include <vector>
 // import randomized character selection function into main function.
 #include "Gettinginput.h"
 #include "Starting_game.h"
 #include "fighting.h"
+#include "Title.h"
 
 using namespace std;
 
 int main() {
+    print_title();
     char input = getting_input();
     Skill s[4];
     Enemy e[5];
@@ -27,7 +30,8 @@ int main() {
     string reply = "Yes";
     while (level < 6 && reply == "Yes") {
         winner = "None";
-        fighting(level, skill_number, job, s, winner, e);
+        vector<int> number_of_skills;
+        fighting(level, skill_number, job, s, winner, e, number_of_skills);
 
         if (winner == "Player") {
             // save the name class level and skill_number
@@ -35,6 +39,26 @@ int main() {
             character.open(C_status);
             character << name << " " << job << " " << level << " " << skill_number;
             character.close();
+            cout << endl;
+            cout << "You have used " << number_of_skills.size() << " skills to defeat the enemy" << endl;
+            for (int i = 0; i < number_of_skills.size(); i++) {
+                if (number_of_skills[i] == 0)
+                    skill_1 += 1;
+                else if (number_of_skills[i] == 1)
+                    skill_2 += 1;
+                else if (number_of_skills[i] == 2)
+                    skill_3 += 1;
+                else if (number_of_skills[i] == 3)
+                    skill_4 += 1;
+            }
+            if (skill_1 != 0)
+                cout << "You have used skill 1 " << skill_1 << " times!" << endl;
+            if (skill_2 != 0)
+                cout << "You have used skill 2 " << skill_2 << " times!" << endl;
+            if (skill_3 != 0)
+                cout << "You have used skill 3 " << skill_3 << " times!" << endl;
+            if (skill_4 != 0)
+                cout << "You have used skill 4 " << skill_4 << " times!" << endl;
         }
 
         else if (winner == "Enemy") {
@@ -42,6 +66,7 @@ int main() {
             cin >> reply;
             cout << endl;
         }
+        number_of_skills.clear();
     }
 
     if (level == 6) {
